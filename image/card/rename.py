ce5bc25846f1bcaec070419f1740d15001d7247a
Arthur Wuterich <ArthurWut@gmail.com>
2015-07-20 14:09:17 -0400

    Adds some checking to the rename script to only rename if there is a difference in the image name. Adds total changed images to output of rename.py

--- image/card/rename.py
+++ image/card/rename.py
@@ -3,13 +3,20 @@
 import os, sys
 
 def transformName(name):
-  return "".join([ i for i in name.replace(u" ","-") if i.isalnum() or i == "-" ])
+  return "%s.jpg" % "".join([ i for i in name.replace(u" ","-") if i.isalnum() or i == "-" ])
 
 def main():
+  totalRenames = 0
   for f in os.listdir("."):
     if f[-3:] in "jpg":
       newName = transformName(f[:-3])
-      os.system( "mv '%s' '%s'" % ( f, "%s.jpg" % newName ) )
 
+      # Move the file if the name has changed
+      if not f == newName:
+        print( "renaming image '%s' => '%s'" % (f, newName) )
+        totalRenames += 1
+        os.system( "mv '%s' '%s'" % ( f, newName ) )
+
+  print( "renamed a total of %d cards" % totalRenames )
 if __name__ == "__main__":
   main()
